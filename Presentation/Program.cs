9996fb6a8b6ef3d4220dc52e3b2ca959c801a3fa
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2023-11-29 17:32:24 +0100

    Error solucionado

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -1,6 +1,5 @@
-﻿using Inventario.Models;
-using Inventario.Service;
-using System.Text;
+﻿using System.Collections.Generic;
+using Inventario.Models;
 List<Movement> Lista= new List<Movement>();
 Product Zapatilla = new Product("Zapa",50,"Calzado",20,Lista);
 
